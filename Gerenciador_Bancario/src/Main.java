619d8298f6d9a6c0e6e119e0bc4469c975244a70
GABRIEL WOLF LEBER <gabriel.leber@alunos.sc.senac.br>
2025-04-16 21:41:06 -0300

    Final

--- Gerenciador_Bancario/src/Main.java
+++ Gerenciador_Bancario/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,43 +8,55 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Gerenciador Bancário: ");
         System.out.println();
-
-
-        //Funciionalidade de Cadastro//
-
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> listaContas = new ArrayList<>();
-        Boolean cadastrarNovo = true;
+        ArrayList<ArrayList<Object>> contas = new ArrayList<>();
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Menu de opções: \n" +
+                    "1 - Cadastrar Conta \n" +
+                    "2 - Listar contas \n" +
+                    "3 - Sair \n" +
+                    "4 - Realizar Depositos \n" +
+                    "5 - Realizar Saques \n" +
+                    "6 - Consultar Saldo \n" +
+                    "7 - Consultar historico de movimentacao \n" +
+                    "8 - Transferir Valor entre contas ");
 
-        while (cadastrarNovo) {
-            System.out.println("Digite Nome e número de conta a serem cadastrados: ");
-            String nomes = scanner.nextLine();
-            listaContas.add(nomes);
+
+            String operacao = scanner.nextLine();
+            if (Objects.equals(operacao, "1")) {
+                System.out.println("Digite o nome da conta");
+                String nomeConta = scanner.nextLine();
+                ArrayList<Object> linha = new ArrayList<>();
+                linha.add(nomeConta);
+                contas.add(linha);
 
 
-            System.out.println("Deseja Cadastrar novo Nome e numero de conta? (True/False)");
-            cadastrarNovo = scanner.nextBoolean();
-            scanner.nextLine();
+            }
+            if (Objects.equals(operacao, "2")) {
+                System.out.println(contas);
+
+
+            }
+            if (Objects.equals(operacao, "4")) {
+                System.out.println("Digite o Valor de deposito desejado: ");
+                String valorDeposito = scanner.nextLine();
+                
+
+            }
+            continuar = !Objects.equals(operacao, "3");
+
 
         }
 
-        System.out.println("Nomes e numeros de contas já cadastrados: ");
-        for (String nomes : listaContas) {
-            System.out.println(nomes);
-        }
-
-        scanner.close();
-
-        // término da Funcionalidade Cadastro//
-
-
-        //Funcionalidade Depósito//
-
-        Scanner scanner1 = new Scanner(System.in);
-        ArrayList<String> listaDeposito = new ArrayList<>();
-
-        }
     }
+}
+
+
+
+
+
+
 
 
 
